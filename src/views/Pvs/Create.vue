718f0414f06b2023404c223103da5904f9e47ea0
Ray0Emma <ahmadifarah22@gmail.com>
2022-09-03 21:41:27 +0100

    fix: fixing required attr

--- src/views/Pvs/Create.vue
+++ src/views/Pvs/Create.vue
@@ -108,13 +108,13 @@ const submit = async () => {
   if (form.semester_name != "") {
     router.push(
       "/pv/show/" +
-        form.filiere_name +
-        "/" +
-        form.semester_name +
-        "/" +
-        form.module_id +
-        "/" +
-        form.time
+      form.filiere_name +
+      "/" +
+      form.semester_name +
+      "/" +
+      form.module_id +
+      "/" +
+      form.time
     );
   }
 };
@@ -127,26 +127,17 @@ const submit = async () => {
         <div class="flex flex-col sm:flex-row items-start justify-between">
           <div class="w-full sm:w-6/12 items-center mb-3 sm:mb-0">
             <div class="flex items-center">
-              <h1
-                class="flex items-center text-2xl xl:text-5xl text-black dark:text-gray-300 font-light -ml-0.5 mb-5"
-              >
+              <h1 class="flex items-center text-2xl xl:text-5xl text-black dark:text-gray-300 font-light -ml-0.5 mb-5">
                 Nouveau Pv
               </h1>
             </div>
           </div>
           <div class="w-full sm:w-6/12">
             <div
-              class="flex flex-wrap flex-col sm:flex-row sm:items-center justify-end sm:space-x-2 sm:rtl:space-x-reverse"
-            >
+              class="flex flex-wrap flex-col sm:flex-row sm:items-center justify-end sm:space-x-2 sm:rtl:space-x-reverse">
               <a
-                class="px-3 py-1.5 cursor-pointer sm:mb-0 rounded-xl text-sm font-medium leading-6 bg-violet-600 hover:bg-violet-700 text-white"
-              >
-                <input
-                  type="file"
-                  ref="input"
-                  @input="upload"
-                  class="absolute opacity-0"
-                />
+                class="px-3 py-1.5 cursor-pointer sm:mb-0 rounded-xl text-sm font-medium leading-6 bg-violet-600 hover:bg-violet-700 text-white">
+                <input type="file" ref="input" @input="upload" class="absolute opacity-0" />
                 Importer l'Excel
               </a>
             </div>
@@ -166,31 +157,18 @@ const submit = async () => {
                 <form @submit.prevent="submit" role="form" class="mb-0">
                   <div class="mb-14">
                     <div class="border-b-2 border-gray-200 pb-4">
-                      <h2
-                        class="lg:text-lg font-medium text-black dark:text-gray-300"
-                      >
+                      <h2 class="lg:text-lg font-medium text-black dark:text-gray-300">
                         Generer Pv
                       </h2>
-                      <span
-                        class="text-sm font-light text-black dark:text-gray-300"
-                      >
+                      <span class="text-sm font-light text-black dark:text-gray-300">
                         Ajouter les informations pour generer un pv .
                       </span>
                     </div>
                     <div class="grid sm:grid-cols-6 gap-x-8 gap-y-6 my-3.5">
-                      <div
-                        class="sm:col-span-3 grid gap-x-8 gap-y-6 grid-rows-2"
-                      >
+                      <div class="sm:col-span-3 grid gap-x-8 gap-y-6 grid-rows-2">
                         <div class="relative sm:col-span-3 required">
-                          <select-input
-                            v-model="form.filiere_id"
-                            @change="sendFil($event), getsem()"
-                            label="Filiere"
-                          >
-                            <option
-                              v-for="filiere in form.filiere"
-                              :value="filiere.id"
-                            >
+                          <select-input v-model="form.filiere_id" @change="sendFil($event), getsem()" label="Filiere">
+                            <option v-for="filiere in form.filiere" :value="filiere.id">
                               {{ filiere.name }}
                             </option>
                           </select-input>
@@ -202,15 +180,8 @@ const submit = async () => {
                           <!---->
                         </div>
                         <div class="relative sm:col-span-3 required">
-                          <select-input
-                            v-model="form.semester_id"
-                            @change="sendSem($event), getmod()"
-                            label="Semester"
-                          >
-                            <option
-                              v-for="semester in form.semester"
-                              :value="semester.id"
-                            >
+                          <select-input v-model="form.semester_id" @change="sendSem($event), getmod()" label="Semester">
+                            <option v-for="semester in form.semester" :value="semester.id">
                               {{ semester.name }}
                             </option>
                           </select-input>
@@ -222,11 +193,9 @@ const submit = async () => {
                           <!---->
                         </div>
                       </div>
-                      <div
-                        class="sm:col-span-3 grid gap-x-8 gap-y-6 grid-rows-2"
-                      >
+                      <div class="sm:col-span-3 grid gap-x-8 gap-y-6 grid-rows-2">
                         <div class="relative sm:col-span-3">
-                          <select-input v-model="form.time" label="Temps">
+                          <select-input v-model="form.time" label="Temps" required>
                             <option value="8">8</option>
                             <option value="10">10</option>
                             <option value="2">2</option>
@@ -239,11 +208,8 @@ const submit = async () => {
                           > -->
                           <!---->
                         </div>
-                        <select-input @change="sendMod($event)" label="Module">
-                          <option
-                            v-for="modul in form.module"
-                            :value="modul.name"
-                          >
+                        <select-input @change="sendMod($event)" label="Module" required>
+                          <option v-for="modul in form.module" :value="modul.name">
                             {{ modul.name }}
                           </option>
                         </select-input>
@@ -252,16 +218,12 @@ const submit = async () => {
                   </div>
                   <div class="mb-14">
                     <div class="flex items-center justify-end sm:col-span-6">
-                      <a
-                        href="/dashboard"
-                        class="px-6 py-1.5 hover:bg-gray-200 hover:text-gray-800 rounded-lg ltr:mr-2 rtl:ml-2"
-                      >
+                      <a href="/dashboard"
+                        class="px-6 py-1.5 hover:bg-gray-200 hover:text-gray-800 rounded-lg ltr:mr-2 rtl:ml-2">
                         Annuler
                       </a>
-                      <button
-                        type="submit"
-                        class="ml-5 relative flex items-center justify-center bg-violet-500 hover:bg-violet-700 px-6 py-1.5 text-base rounded-lg disabled:bg-violet-100"
-                      >
+                      <button type="submit"
+                        class="ml-5 relative flex items-center justify-center bg-violet-500 hover:bg-violet-700 px-6 py-1.5 text-base rounded-lg disabled:bg-violet-100">
                         <span class=""> Generer </span>
                       </button>
                     </div>
