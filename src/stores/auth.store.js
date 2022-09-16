e219b72db81dea08779824d36f797155b441d920
Ray0Emma <ahmadifarah22@gmail.com>
2022-09-16 15:34:11 +0100

    feat: added authority condition

--- src/stores/auth.store.js
+++ src/stores/auth.store.js
@@ -1,54 +1,51 @@
-import { defineStore } from 'pinia';
+import { defineStore } from "pinia";
 
-import { fetchWrapper } from '@/helpers/fetch-wrapper';
-import router from '@/router';
+import { fetchWrapper } from "@/helpers/fetch-wrapper";
+import router from "@/router";
 // import { router } from '@/router';
 // import { useAlertStore } from '@/stores';
 
-const baseUrl = 'http://localhost:8080';
+const baseUrl = "http://localhost:8080";
 
 export const useAuthStore = defineStore({
-    id: 'auth',
-    state: () => ({
-        // initialize state from local storage to enable user to stay logged in
-        user: localStorage.getItem('user'),
-        returnUrl: null
-    }),
-    actions: {
-         async login(username, password) {
+  id: "auth",
+  state: () => ({
+    // initialize state from local storage to enable user to stay logged in
+    user: localStorage.getItem("user"),
+    returnUrl: null,
+  }),
+  actions: {
+    async login(username, password) {
+      try {
+        const user = await fetchWrapper.post(`${baseUrl}/login`, {
+          username: username,
+          password: password,
+        });
 
-            try {
-                 const user = await fetchWrapper.post(`${baseUrl}/login`,  {
-                    username: username,
-                    password: password
-                  } ) 
-                
-                  this.user = user;
-                  console.log(`login ${JSON.stringify(user)}`)
-                if (this.user) {
-                    
+        this.user = user;
+        console.log(`login ${JSON.stringify(user.role)}`);
+        if (this.user.role) {
+          this.user.role.forEach((element) => {
+            if (element.authority == "admin") {
+              // store user details and jwt in local storage to keep user logged in between page refreshes
+              localStorage.setItem("user", JSON.stringify(this.user));
 
-                // update pinia state
-                // this.user = user;
-
-
-
-                // store user details and jwt in local storage to keep user logged in between page refreshes
-                localStorage.setItem('user', JSON.stringify(this.user));
-
-                // redirect to previous url or default to home page
-                router.push(this.returnUrl || '/dashboard');
-                  }
-                
-            } catch (error) {
-                // const alertStore = useAlertStore();
-                alert(error);
+              // redirect to previous url or default to home page
+              router.push(this.returnUrl || "/dashboard");
+            } else {
+              alert("this user is not authorized");
             }
-        },
-        logout() {
-            this.user = null;
-            localStorage.removeItem('user');
-            router.push('/');
+          });
         }
-    }
+      } catch (error) {
+        // const alertStore = useAlertStore();
+        alert(error);
+      }
+    },
+    logout() {
+      this.user = null;
+      localStorage.removeItem("user");
+      router.push("/");
+    },
+  },
 });
