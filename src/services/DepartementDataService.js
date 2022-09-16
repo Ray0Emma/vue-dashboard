b9309b7ca02cf02845786c3e3ea58b83c42a4241
Ray0Emma <ahmadifarah22@gmail.com>
2022-09-16 17:19:28 +0100

    fix: fixed authorization

--- src/services/DepartementDataService.js
+++ src/services/DepartementDataService.js
@@ -1,43 +1,63 @@
-/* eslint-disable */
 import axios from "axios";
 
-class BaseDataService {
+const  user  = JSON.parse(localStorage.getItem("user"));
 
+class BaseDataService {
   API_URL = "http://localhost:8080";
 
   retrieveAllData(INSTRUCTOR) {
-    return axios.get(`${this.API_URL}/${INSTRUCTOR}`);
+    return axios.get(`${this.API_URL}/${INSTRUCTOR}`, {
+      headers: { Authorization: `Bearer ${user.access_token}` },
+    });
   }
 
   deleteData(INSTRUCTOR, id) {
-    return axios.delete(`${this.API_URL}/${INSTRUCTOR}/${id}`);
+    return axios.delete(`${this.API_URL}/${INSTRUCTOR}/${id}`, {
+      headers: { Authorization: `Bearer ${user.access_token}` },
+    });
   }
 
   retrieveData(INSTRUCTOR, id) {
-    return axios.get(`${this.API_URL}/${INSTRUCTOR}/${id}`);
+    return axios.get(`${this.API_URL}/${INSTRUCTOR}/${id}`, {
+      headers: { Authorization: `Bearer ${user.access_token}` },
+    });
   }
   updateData(INSTRUCTOR, id, course) {
-    return axios.patch(`${this.API_URL}/${INSTRUCTOR}/${id}`, course);
+    return axios.patch(`${this.API_URL}/${INSTRUCTOR}/${id}`, course, {
+      headers: { Authorization: `Bearer ${user.access_token}` },
+    });
   }
 
   createData(INSTRUCTOR, course) {
-    return axios.post(`${this.API_URL}/${INSTRUCTOR}/`, course);
+    return axios.post(`${this.API_URL}/${INSTRUCTOR}/`, course, {
+      headers: { Authorization: `Bearer ${user.access_token}` },
+    });
   }
 
   createPv(INSTRUCTOR, filiere, semester, modul, time) {
-    return axios.get(`${this.API_URL}/${INSTRUCTOR}/${filiere}/${semester}/${modul}/${time}`);
+    return axios.get(
+      `${this.API_URL}/${INSTRUCTOR}/${filiere}/${semester}/${modul}/${time}`, {
+        headers: { Authorization: `Bearer ${user.access_token}` },
+      }
+    );
   }
 
   uploadFile(INSTRUCTOR, file) {
-    return axios.post(`${this.API_URL}/${INSTRUCTOR}/uploadFile`, file);
+    return axios.post(`${this.API_URL}/${INSTRUCTOR}/uploadFile`, file, {
+      headers: { Authorization: `Bearer ${user.access_token}` },
+    });
   }
 
   uploadExcel(INSTRUCTOR, file) {
-    return axios.post(`${this.API_URL}/${INSTRUCTOR}/upload`, file);
+    return axios.post(`${this.API_URL}/${INSTRUCTOR}/upload`, file, {
+      headers: { Authorization: `Bearer ${user.access_token}` },
+    });
   }
 
-  orderPv(INSTRUCTOR, etudiant,pv) {
-    return axios.get(`${this.API_URL}/${INSTRUCTOR}/order/${etudiant}/${pv}`);
+  orderPv(INSTRUCTOR, etudiant, pv) {
+    return axios.get(`${this.API_URL}/${INSTRUCTOR}/order/${etudiant}/${pv}`, {
+      headers: { Authorization: `Bearer ${user.access_token}` },
+    });
   }
 }
 
