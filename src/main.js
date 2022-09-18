10fb7879a22922fd145b5db8c36fe5f876e294d4
Ray0Emma <ahmadifarah22@gmail.com>
2022-09-18 13:15:56 +0100

    fix: fixed routes

--- src/main.js
+++ src/main.js
@@ -6,6 +6,8 @@ import router from "./router";
 import { useMainStore } from "@/stores/main.js";
 import { useStyleStore } from "@/stores/style.js";
 import { useLayoutStore } from "@/stores/layout.js";
+
+import { useAuthStore } from "@/stores/auth.store";
 import { darkModeKey, styleKey } from "@/config.js";
 
 import "./css/main.css";
@@ -20,6 +22,7 @@ createApp(App).use(router).use(pinia).mount("#app");
 const mainStore = useMainStore(pinia);
 const styleStore = useStyleStore(pinia);
 const layoutStore = useLayoutStore(pinia);
+const AuthStore = useAuthStore(pinia);
 
 /* Fetch sample data */
 // mainStore.fetch("clients");
@@ -52,3 +55,26 @@ router.afterEach((to) => {
     ? `${to.meta.title} — ${defaultDocumentTitle}`
     : defaultDocumentTitle;
 });
+
+let isAuthenticated = localStorage.getItem('user');
+router.beforeEach((to, from, next) => {
+  // if (to.name !== "Login" && !isAuthenticated) next({ name: "Login" });
+  isAuthenticated = localStorage.getItem('user');
+  if (from.name !== "Login" && to.name !== "Login" && !isAuthenticated) 
+  // {
+    // Not logged in? You get booted to /login
+    setTimeout(()=>next({ name: "Login" }),3000);
+  
+  
+  else 
+    next()
+  
+});
+
+router.beforeEach((to, from, next) => {
+  isAuthenticated = localStorage.getItem('user');
+  if (to.name === "Login" && isAuthenticated) next({ name: "dashboard" })
+  else next()
+
+});
+
--- src/services/DepartementDataService.js
+++ src/services/DepartementDataService.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { useAuthStore } from '@/stores/auth.store';
 
 const  user  = JSON.parse(localStorage.getItem("user"));
 
@@ -7,50 +8,50 @@ class BaseDataService {
 
   retrieveAllData(INSTRUCTOR) {
     return axios.get(`${this.API_URL}/${INSTRUCTOR}`, {
-      headers: { Authorization: `Bearer ${user.access_token}` },
+      headers: { Authorization: `Bearer ${!!user?.access_token ? user.access_token :''}` },
     });
   }
 
   deleteData(INSTRUCTOR, id) {
     return axios.delete(`${this.API_URL}/${INSTRUCTOR}/${id}`, {
-      headers: { Authorization: `Bearer ${user.access_token}` },
+      headers: { Authorization: `Bearer ${!!user?.access_token ? user.access_token :''}` },
     });
   }
 
   retrieveData(INSTRUCTOR, id) {
     return axios.get(`${this.API_URL}/${INSTRUCTOR}/${id}`, {
-      headers: { Authorization: `Bearer ${user.access_token}` },
+      headers: { Authorization: `Bearer ${!!user?.access_token ? user.access_token :''}` },
     });
   }
   updateData(INSTRUCTOR, id, course) {
     return axios.patch(`${this.API_URL}/${INSTRUCTOR}/${id}`, course, {
-      headers: { Authorization: `Bearer ${user.access_token}` },
+      headers: { Authorization: `Bearer ${!!user?.access_token ? user.access_token :''}` },
     });
   }
 
   createData(INSTRUCTOR, course) {
     return axios.post(`${this.API_URL}/${INSTRUCTOR}/`, course, {
-      headers: { Authorization: `Bearer ${user.access_token}` },
+      headers: { Authorization: `Bearer ${!!user?.access_token ? user.access_token :''}` },
     });
   }
 
   createPv(INSTRUCTOR, filiere, semester, modul, time) {
     return axios.get(
       `${this.API_URL}/${INSTRUCTOR}/${filiere}/${semester}/${modul}/${time}`, {
-        headers: { Authorization: `Bearer ${user.access_token}` },
+        headers: { Authorization: `Bearer ${!!user?.access_token ? user.access_token :''}` },
       }
     );
   }
 
   uploadFile(INSTRUCTOR, file) {
     return axios.post(`${this.API_URL}/${INSTRUCTOR}/uploadFile`, file, {
-      headers: { Authorization: `Bearer ${user.access_token}` },
+      headers: { Authorization: `Bearer ${!!user?.access_token ? user.access_token :''}` },
     });
   }
 
   uploadExcel(INSTRUCTOR, file) {
     return axios.post(`${this.API_URL}/${INSTRUCTOR}/upload`, file, {
-      headers: { Authorization: `Bearer ${user.access_token}` },
+      headers: { Authorization: `Bearer ${!!user?.access_token ? user.access_token :''}` },
     });
   }
 
@@ -60,5 +61,17 @@ class BaseDataService {
     });
   }
 }
+
+axios.interceptors.response.use(response => {
+  return response;
+}, error => {
+  // if ([401, 403].includes(error.response.status)) {
+  //     // console.log('token expired',error.response)
+  //     useAuthStore().logout();
+  //     /* THIS WORKS BUT BREAKS THE LOGIN ERROR HANDLING */
+
+  // }
+  // return Promise.reject(error);
+});
 
 export default new BaseDataService();
